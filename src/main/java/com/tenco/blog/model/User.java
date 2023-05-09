75f1439dbea8a91843698aca523f4ca0fb7e2db8
DESKTOP-8N3GG4O\GGG <kmg1151@naver.com>
2023-05-09 18:26:54 +0900

    테이블 생성 및 JPA 테스트

--- src/main/java/com/tenco/blog/model/User.java
+++ src/main/java/com/tenco/blog/model/User.java
@@ -2,20 +2,32 @@ package com.tenco.blog.model;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
 @Entity
 public class User {
-	
+
 	// 기본키 매핑
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id // pk 설정
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private int id;
+	@Column(nullable = false, length = 30)
 	private String username;
+	@Column(nullable = false, length = 100)
 	private String password;
+	@Column(nullable = false, length = 50)
 	private String email;
+	@CreationTimestamp // 자동 시간 입력 now() 
 	private Timestamp createdDate;
+	@ColumnDefault("'user'") // 문자열 타입이라고 명시는 ''(홑따옴표)
+	private String role; // 데이터의 범주화 : user, admin, manager
+	
 }
+	
